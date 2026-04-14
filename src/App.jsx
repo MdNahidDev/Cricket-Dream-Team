802a84f8ec3fcbdef91f897dcccf31714d848a99
MdNahidDev <mdnahidhasan.tech@gmail.com>
2026-04-14 16:18:35 +0600

    Added available and selected players button

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,19 @@ function App() {
     <>
       <Navbar></Navbar>
 
+      <div className="max-w-[1200] w-11/12 mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-2xl">Available players</h1>
+
+        <div className="font-bold">
+          <button className="py-3 px-4 border border-gray-400 border-r-0 rounded-l-2xl bg-[#E7FE29]">
+            Available players
+          </button>
+          <button className="py-3 px-4 border border-gray-400 border-l-0 rounded-r-2xl">
+            Selected <span>0</span>
+          </button>
+        </div>
+      </div>
+
       <Suspense
         fallback={<span className="loading loading-ring loading-xl"></span>}
       >
